c3b7a2466eb25551df1e118742d13e647e1fed9d
Stefan <stefan.jovanavich@gmail.com>
2021-11-09 11:54:07 +0100

    Create Hero Model class

---
+++ app/src/main/java/com/example/borutoapp/domain/model/Hero.kt
@@ -0,0 +1,21 @@
+package com.example.borutoapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.borutoapp.util.Constants.HERO_DATABASE_TABLE
+
+@Entity(tableName = HERO_DATABASE_TABLE)
+data class Hero(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val image: String,
+    val about: String,
+    val rating: Double,
+    val power: Int,
+    val month: String,
+    val day: String,
+    val family: List<String>,
+    val abilities: List<String>,
+    val natureTypes: List<String>
+)
--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val DETAILS_ARGUMENT_KEY = "heroId"
 
+    const val HERO_DATABASE_TABLE = "hero_table"
+
 }
