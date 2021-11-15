0906a96b91746678e2a6371c6889b7341b29b6ce
Stefan <stefan.jovanavich@gmail.com>
2021-11-15 16:42:01 +0100

    Create HomeTopBar

--- app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,13 @@
 package com.example.borutoapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }
+    ) {}
 }
--- app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
@@ -36,3 +36,11 @@ val Colors.inactiveIndicatorColor
 val Colors.buttonBackgroundColor
     @Composable
     get() = if (isLight) Purple500 else Purple700
+
+val Colors.topAppBarContentColor: Color
+    @Composable
+    get() = if (isLight) Color.White else LightGray
+
+val Colors.topAppBarBackgroundColor: Color
+    @Composable
+    get() = if (isLight) Purple500 else Color.Black
