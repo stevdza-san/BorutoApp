2303e9f9bf39bfd50beea0864d2de2a061c4e427
Stefan <stefan.jovanavich@gmail.com>
2021-11-25 12:56:45 +0100

    Design RatingWidget - Calculation

--- app/src/main/java/com/example/borutoapp/presentation/components/RatingWidget.kt
+++ app/src/main/java/com/example/borutoapp/presentation/components/RatingWidget.kt
@@ -1,9 +1,11 @@
 package com.example.borutoapp.presentation.components
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
@@ -14,8 +16,10 @@ import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.graphics.vector.PathParser
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.borutoapp.R
+import com.example.borutoapp.ui.theme.EXTRA_SMALL_PADDING
 import com.example.borutoapp.ui.theme.LightGray
 import com.example.borutoapp.ui.theme.StarColor
 
@@ -23,8 +27,11 @@ import com.example.borutoapp.ui.theme.StarColor
 fun RatingWidget(
     modifier: Modifier,
     rating: Double,
-    scaleFactor: Float = 3f
+    scaleFactor: Float = 3f,
+    spaceBetween: Dp = EXTRA_SMALL_PADDING
 ) {
+    val result = calculateStars(rating = rating)
+
     val starPathString = stringResource(id = R.string.star_path)
     val starPath = remember {
         PathParser().parsePathString(pathData = starPathString).toPath()
@@ -33,7 +40,38 @@ fun RatingWidget(
         starPath.getBounds()
     }
 
-    FilledStar(starPath = starPath, starPathBounds = starPathBounds, scaleFactor = scaleFactor)
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(spaceBetween)
+    ) {
+        result["filledStars"]?.let {
+            repeat(it) {
+                FilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+        result["halfFilledStars"]?.let {
+            repeat(it) {
+                HalfFilledStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+        result["emptyStars"]?.let {
+            repeat(it) {
+                EmptyStar(
+                    starPath = starPath,
+                    starPathBounds = starPathBounds,
+                    scaleFactor = scaleFactor
+                )
+            }
+        }
+    }
 
 }
 
@@ -44,7 +82,7 @@ fun FilledStar(
     scaleFactor: Float
 ) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
             val pathHeight = starPathBounds.height
@@ -68,7 +106,7 @@ fun HalfFilledStar(
     scaleFactor: Float
 ) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
             val pathHeight = starPathBounds.height
@@ -101,7 +139,7 @@ fun EmptyStar(
     scaleFactor: Float
 ) {
     Canvas(modifier = Modifier.size(24.dp)) {
-        val canvasSize = this.size
+        val canvasSize = size
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
             val pathHeight = starPathBounds.height
@@ -116,6 +154,44 @@ fun EmptyStar(
             }
         }
     }
+}
+
+@Composable
+fun calculateStars(rating: Double): Map<String, Int> {
+    val maxStars by remember { mutableStateOf(5) }
+    var filledStars by remember { mutableStateOf(0) }
+    var halfFilledStars by remember { mutableStateOf(0) }
+    var emptyStars by remember { mutableStateOf(0) }
+
+    LaunchedEffect(key1 = rating) {
+        val (firstNumber, lastNumber) = rating.toString()
+            .split(".")
+            .map { it.toInt() }
+
+        if (firstNumber in 0..5 && lastNumber in 0..9) {
+            filledStars = firstNumber
+            if (lastNumber in 1..5) {
+                halfFilledStars++
+            }
+            if (lastNumber in 6..9) {
+                filledStars++
+            }
+            if (firstNumber == 5 && lastNumber > 0) {
+                emptyStars = 5
+                filledStars = 0
+                halfFilledStars = 0
+            }
+        } else {
+            Log.d("RatingWidget", "Invalid rating number.")
+        }
+    }
+
+    emptyStars = maxStars - (filledStars + halfFilledStars)
+    return mapOf(
+        "filledStars" to filledStars,
+        "halfFilledStars" to halfFilledStars,
+        "emptyStars" to emptyStars
+    )
 }
 
 @Composable
--- app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.example.borutoapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.borutoapp.presentation.components.RatingWidget
+import com.example.borutoapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
@@ -15,5 +19,10 @@ fun HomeScreen(
         topBar = {
             HomeTopBar(onSearchClicked = {})
         }
-    ) {}
+    ) {
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING),
+            rating = 4.4
+        )
+    }
 }
