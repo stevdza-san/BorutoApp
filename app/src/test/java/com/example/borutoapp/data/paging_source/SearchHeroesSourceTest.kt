2b3559a8a4bfbd9a3927633efe08536a3688dabc
Stefan <stefan.jovanavich@gmail.com>
2021-12-08 12:25:42 +0100

    Testing - Create SearchHeroesSourceTest

---
+++ app/src/test/java/com/example/borutoapp/data/paging_source/SearchHeroesSourceTest.kt
@@ -0,0 +1,155 @@
+package com.example.borutoapp.data.paging_source
+
+import androidx.paging.PagingSource.LoadParams
+import androidx.paging.PagingSource.LoadResult
+import com.example.borutoapp.data.remote.BorutoApi
+import com.example.borutoapp.data.remote.FakeBorutoApi
+import com.example.borutoapp.domain.model.Hero
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+@ExperimentalCoroutinesApi
+class SearchHeroesSourceTest {
+
+    private lateinit var borutoApi: BorutoApi
+    private lateinit var heroes: List<Hero>
+
+    @Before
+    fun setup() {
+        borutoApi = FakeBorutoApi()
+        heroes = listOf(
+            Hero(
+                id = 1,
+                name = "Sasuke",
+                image = "",
+                about = "",
+                rating = 5.0,
+                power = 0,
+                month = "",
+                day = "",
+                family = listOf(),
+                abilities = listOf(),
+                natureTypes = listOf()
+            ),
+            Hero(
+                id = 2,
+                name = "Naruto",
+                image = "",
+                about = "",
+                rating = 5.0,
+                power = 0,
+                month = "",
+                day = "",
+                family = listOf(),
+                abilities = listOf(),
+                natureTypes = listOf()
+            ),
+            Hero(
+                id = 3,
+                name = "Sakura",
+                image = "",
+                about = "",
+                rating = 5.0,
+                power = 0,
+                month = "",
+                day = "",
+                family = listOf(),
+                abilities = listOf(),
+                natureTypes = listOf()
+            )
+        )
+    }
+
+    @Test
+    fun `Search api with existing hero name, expect single hero result, assert LoadResult_Page`() =
+        runBlockingTest {
+            val heroSource = SearchHeroesSource(borutoApi = borutoApi, query = "Sasuke")
+            assertEquals<LoadResult<Int, Hero>>(
+                expected = LoadResult.Page(
+                    data = listOf(heroes.first()),
+                    prevKey = null,
+                    nextKey = null
+                ),
+                actual = heroSource.load(
+                    LoadParams.Refresh(
+                        key = null,
+                        loadSize = 3,
+                        placeholdersEnabled = false
+                    )
+                )
+            )
+        }
+
+    @Test
+    fun `Search api with existing hero name, expect multiple hero result, assert LoadResult_Page`() =
+        runBlockingTest {
+            val heroSource = SearchHeroesSource(borutoApi = borutoApi, query = "Sa")
+            assertEquals<LoadResult<Int, Hero>>(
+                expected = LoadResult.Page(
+                    data = listOf(heroes.first(), heroes[2]),
+                    prevKey = null,
+                    nextKey = null
+                ),
+                actual = heroSource.load(
+                    LoadParams.Refresh(
+                        key = null,
+                        loadSize = 3,
+                        placeholdersEnabled = false
+                    )
+                )
+            )
+        }
+
+    @Test
+    fun `Search api with empty hero name, assert empty heroes list and LoadResult_Page`() =
+        runBlockingTest {
+            val heroSource = SearchHeroesSource(borutoApi = borutoApi, query = "")
+            val loadResult = heroSource.load(
+                LoadParams.Refresh(
+                    key = null,
+                    loadSize = 3,
+                    placeholdersEnabled = false
+                )
+            )
+
+            val result = borutoApi.searchHeroes("").heroes
+
+            assertTrue { result.isEmpty() }
+            assertTrue { loadResult is LoadResult.Page }
+        }
+
+    @Test
+    fun `Search api with non_existing hero name, assert empty heroes list and LoadResult_Page`() =
+        runBlockingTest {
+            val heroSource = SearchHeroesSource(borutoApi = borutoApi, query = "Unknown")
+            val loadResult = heroSource.load(
+                LoadParams.Refresh(
+                    key = null,
+                    loadSize = 3,
+                    placeholdersEnabled = false
+                )
+            )
+
+            val result = borutoApi.searchHeroes("Unknown").heroes
+
+            assertTrue { result.isEmpty() }
+            assertTrue { loadResult is LoadResult.Page }
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
