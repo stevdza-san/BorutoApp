0b2b05a3a16c7297a64f4872fa7e003277c73a0e
Stefan <stefan.jovanavich@gmail.com>
2021-12-09 20:20:10 +0100

    Testing - Testing RatingWidget

--- app/src/main/java/com/example/borutoapp/presentation/components/RatingWidget.kt
+++ app/src/main/java/com/example/borutoapp/presentation/components/RatingWidget.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.graphics.vector.PathParser
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -81,7 +83,12 @@ fun FilledStar(
     starPathBounds: Rect,
     scaleFactor: Float
 ) {
-    Canvas(modifier = Modifier.size(24.dp)) {
+    Canvas(modifier = Modifier
+        .size(24.dp)
+        .semantics {
+            contentDescription = "FilledStar"
+        }
+    ) {
         val canvasSize = size
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
@@ -105,7 +112,12 @@ fun HalfFilledStar(
     starPathBounds: Rect,
     scaleFactor: Float
 ) {
-    Canvas(modifier = Modifier.size(24.dp)) {
+    Canvas(modifier = Modifier
+        .size(24.dp)
+        .semantics {
+            contentDescription = "HalfFilledStar"
+        }
+    ) {
         val canvasSize = size
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
@@ -138,7 +150,12 @@ fun EmptyStar(
     starPathBounds: Rect,
     scaleFactor: Float
 ) {
-    Canvas(modifier = Modifier.size(24.dp)) {
+    Canvas(modifier = Modifier
+        .size(24.dp)
+        .semantics {
+            contentDescription = "EmptyStar"
+        }
+    ) {
         val canvasSize = size
         scale(scale = scaleFactor) {
             val pathWidth = starPathBounds.width
