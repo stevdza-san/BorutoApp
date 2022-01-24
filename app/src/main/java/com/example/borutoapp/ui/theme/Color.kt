16090ca5597f45f93217b2ac1bb751eeec4c55de
Stefan <stefan.jovanavich@gmail.com>
2022-01-24 16:11:13 +0100

    Update #1 Code Cleanup

--- app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/borutoapp/ui/theme/Color.kt
@@ -1,7 +1,6 @@
 package com.example.borutoapp.ui.theme
 
 import androidx.compose.material.Colors
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
@@ -18,38 +17,29 @@ val ShimmerMediumGray = Color(0xFFE3E3E3)
 val ShimmerDarkGray = Color(0xFF1D1D1D)
 
 val Colors.statusBarColor
-    @Composable
     get() = if (isLight) Purple700 else Color.Black
 
 val Colors.welcomeScreenBackgroundColor
-    @Composable
     get() = if (isLight) Color.White else Color.Black
 
 val Colors.titleColor
-    @Composable
     get() = if (isLight) DarkGray else LightGray
 
 val Colors.descriptionColor
-    @Composable
     get() = if (isLight) DarkGray.copy(alpha = 0.5f)
     else LightGray.copy(alpha = 0.5f)
 
 val Colors.activeIndicatorColor
-    @Composable
     get() = if (isLight) Purple500 else Purple700
 
 val Colors.inactiveIndicatorColor
-    @Composable
     get() = if (isLight) LightGray else DarkGray
 
 val Colors.buttonBackgroundColor
-    @Composable
     get() = if (isLight) Purple500 else Purple700
 
 val Colors.topAppBarContentColor: Color
-    @Composable
     get() = if (isLight) Color.White else LightGray
 
 val Colors.topAppBarBackgroundColor: Color
-    @Composable
     get() = if (isLight) Purple500 else Color.Black
